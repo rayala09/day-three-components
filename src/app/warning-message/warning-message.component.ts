43b258923f6f9341494ffec976e55ccbe08949b3
Moises <moyayala12@gmail.com>
2018-06-07 15:24:21 -0700

    day-three-exercises

---
+++ src/app/warning-message/warning-message.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-message',
+  template: `
+  <h2> warning message </h2>
+  `,
+  styleUrls: ['./warning-message.component.css']
+})
+export class WarningMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
